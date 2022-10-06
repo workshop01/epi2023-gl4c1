6385bccb72e30a3f56b3c27df7c59e7413674e02
Workshop01 <nourhene.coding@gmail.com>
2022-10-06 09:01:43 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+var today = new Date()
+var time = today.toLocaleDateString()
+var x = 'test'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <div style={{color : 'red'}} className="title" htmlFor="test">
+    {time}
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root2 = ReactDOM.createRoot(document.getElementById('epi'));
+root2.render(
+ <>
+ test from root 2
+ </>
+);
+
+
